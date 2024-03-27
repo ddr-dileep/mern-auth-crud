b97d03249c338b1707a01612c462b428bbe78f5e
Dileep kumar <ddr.dileepraloti@gmail.com>
2024-03-28 02:59:11 +0530

    feat : added 404 page

--- front-end/src/AllRoutes.jsx
+++ front-end/src/AllRoutes.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/home-page/HomePage";
 import LoginPage from "./pages/login-page/LoginPage";
 import RegisterPage from "./pages/register-page/RegisterPage";
+import NotFound from "./pages/not-found-page/NotFound";
 
 export const AllRoutes = () => {
   return (
@@ -9,7 +10,7 @@ export const AllRoutes = () => {
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<RegisterPage />} />
-      {/* <Route path="*" element={<NotFound />} /> */}
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
