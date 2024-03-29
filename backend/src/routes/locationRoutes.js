e3021d33c4c4623c84ae369de1e6dda3fb89b70f
Dileep kumar <ddr.dileepraloti@gmail.com>
2024-03-29 07:43:56 +0530

    fix: api for authenticate user only

--- backend/src/routes/locationRoutes.js
+++ backend/src/routes/locationRoutes.js
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import locationControllers from "../controllers/locationControllers.js";
+import { verifyUserMiddleware } from "../middlewares/authMiddleware.js";
 
 const locationRouter = Router();
 
-locationRouter.get("/near-by", locationControllers.getNearByUser);
+locationRouter.get(
+  "/near-by",
+  verifyUserMiddleware,
+  locationControllers.getNearByUser
+);
 
 export default locationRouter;
