3869cc531a549cef7542f3c751bb37f305af8c3c
Dileep kumar <ddr.dileepraloti@gmail.com>
2024-03-29 09:35:49 +0530

    fix : backend live url

--- front-end/src/api/axiosConfigs.js
+++ front-end/src/api/axiosConfigs.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL: "https://mern-auth-crud-be.onrender.com/api/v1",
   withCredentials: true,
 });
 
