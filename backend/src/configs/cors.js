7de37d9b736d982edda12844fa23a7169f928aaa
Dileep kumar <ddr.dileepraloti@gmail.com>
2024-03-29 10:49:38 +0530

    fix : cors

--- backend/src/configs/cors.js
+++ backend/src/configs/cors.js
@@ -1,4 +1,8 @@
-const allowedOrigins = ["http://localhost:5173", "https://mern-auth-crud-be.onrender.com"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mern-auth-crud-be.onrender.com",
+  "https://mern-auth-crud-frontend.onrender.com",
+];
 
 export const corsOptions = {
   origin: allowedOrigins,
